334c1da14cb249232e9532d4006385da18bdf28b
Yves <vyves246@gmail.com>
2026-04-27 10:25:36 -0700

    variable name change

--- fcfs.py
+++ fcfs.py
@@ -23,22 +23,22 @@ def results(completed_orders):
     print("\n WAFFLE HOUSE ORDER LOG (FCFS)")
     print(f"{'Booth':<8} | {'Arrival':<8} | {'Cook Time':<10} | {'Served':<8} | {'Total Stay':<10} | {'Wait'}")
     
-    total_hangry_time = 0
+    total_waiting_time = 0
     total_stay_time = 0
     
     for order in completed_orders:
-        total_hangry_time += order['hangry_wait_time']
+        total_waiting_time += order['hangry_wait_time']
         total_stay_time += order['total_diner_time']
         
         print(f"Booth {order['id']:<2} | {order['walk_in_time']:<8} | {order['cooking_duration']:<10} | "
               f"{order['plate_served']:<8} | {order['total_diner_time']:<10} | {order['hangry_wait_time']}")
 
-    avg_wait = total_hangry_time / len(completed_orders)
+    avg_wait = total_waiting_time / len(completed_orders)
     avg_stay = total_stay_time / len(completed_orders)
     
     print("\n")
     print("--- ORDER UP! ---")
-    print(f"Average 'Hangry' Wait: {round(avg_wait, 2)} minutes")
+    print(f"Average Wait: {round(avg_wait, 2)} minutes")
     print(f"Average Total Stay:   {round(avg_stay, 2)} minutes")
 
 if __name__ == "__main__":
